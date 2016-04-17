1999d4f0e8e771853abc4b21be94e4ac88322aef
Anton Bely <toshabely@gmail.com>
2016-04-17 23:18:39 +0300

    Create first bean for JSF.

--- src/main/java/com/bsu/bely/medical/bean/MainBean.java
+++ src/main/java/com/bsu/bely/medical/bean/MainBean.java
@@ -1,7 +1,17 @@
 package com.bsu.bely.medical.bean;
 
-/**
- * Created by anton on 17.4.16.
- */
+//import javax.faces.bean.ManagedBean;
+
+//@ManagedBean(name = "main")
 public class MainBean {
+
+    private String value = "Value from MainBean.";
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
