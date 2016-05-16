af095fbb06229b0c1bbed26e8c20dd4c8e289688
Anton Bely <toshabely@gmail.com>
2016-05-16 12:03:58 +0300

    Create line-chart for temperature.

--- src/main/java/com/bsu/bely/medical/bean/ThermalSheetBean.java
+++ src/main/java/com/bsu/bely/medical/bean/ThermalSheetBean.java
@@ -4,6 +4,7 @@ import com.bsu.bely.medical.entity.Patient;
 import com.bsu.bely.medical.entity.ThermalSheet;
 import com.bsu.bely.medical.service.PatientService;
 import com.bsu.bely.medical.service.ThermalSheetService;
+import org.primefaces.model.chart.*;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -13,8 +14,11 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @ViewScoped
 @ManagedBean(name = "thermalSheetBean")
@@ -31,16 +35,22 @@ public class ThermalSheetBean {
     private List<Patient> patientList;
     private List<ThermalSheet> thermalSheetList;
 
+    private LineChartModel lineModel;
+    private boolean showLineModel = false;
+
     @PostConstruct
     private void init() {
         thermalSheet = new ThermalSheet();
         patientList = patientService.getAll();
         thermalSheetList = new ArrayList<>();
+        createLineModel();
     }
 
     public void applyFilter() {
         if (selectedPatient != null) {
             thermalSheetList = thermalSheetService.getThermalSheetsByPatientId(selectedPatient.getId());
+            addSeriesToLineChart();
+            showLineModel = true;
         }
     }
 
@@ -54,6 +64,29 @@ public class ThermalSheetBean {
         if (value == null || (double) value < 34.5 || (double) value > 42.5) {
             throw new ValidatorException(new FacesMessage("Wrong temperature"));
         }
+    }
+
+    private void createLineModel() {
+        lineModel = new LineChartModel();
+        lineModel.setShowPointLabels(true);
+        Axis yAxis = lineModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Temperature");
+        yAxis.setMin(34);
+        yAxis.setMax(43);
+
+        DateAxis dateAxis = new DateAxis("Dates");
+        dateAxis.setTickAngle(-50);
+        dateAxis.setTickFormat("%b %#d, %y %H:%M");
+
+        lineModel.getAxes().put(AxisType.X, dateAxis);
+    }
+
+    private void addSeriesToLineChart() {
+        LineChartSeries series = new LineChartSeries();
+        for (ThermalSheet thermal : thermalSheetList) {
+            series.set(thermal.getDate().getTime(), thermal.getTemperature());
+        }
+        lineModel.addSeries(series);
     }
 
     public ThermalSheet getThermalSheet() {
@@ -94,5 +127,17 @@ public class ThermalSheetBean {
 
     public void setThermalSheetService(ThermalSheetService thermalSheetService) {
         this.thermalSheetService = thermalSheetService;
+    }
+
+    public LineChartModel getLineModel() {
+        return lineModel;
+    }
+
+    public void setLineModel(LineChartModel lineModel) {
+        this.lineModel = lineModel;
+    }
+
+    public boolean isShowLineModel() {
+        return showLineModel;
     }
 }
